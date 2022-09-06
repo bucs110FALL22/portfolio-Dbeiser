eb5d913b48bd25f23af1448cdec46cd79ba1c514
Daniel Beiser <112429959+Dbeiser@users.noreply.github.com>
2022-09-06 21:33:42 +0000

    I finished the "working with data" exercise

--- ch01/exercises/simple.py
+++ ch01/exercises/simple.py
@@ -8,10 +8,13 @@
 # print(10 % 10)
 # print(0 % 10)
 # print(10 / 15)
-#
-rate = float(input("what is the Euro to dollar exchange rate? "))
-amount = float(input("How much would you like to exchange?"))
+#the issue with (10/15) is that the answer is a repeating decimal but it ends in the code.
+rate = float(input("what is the Euro to US Dollar conversion rate? "))
+
+amount = float(input("How much would you like to exchange? "))
+
 total = rate * amount
+
 result = total - 3
-print("your exchanged amount is")
-print(result)
+
+print("your exchanged amount is $", result)
